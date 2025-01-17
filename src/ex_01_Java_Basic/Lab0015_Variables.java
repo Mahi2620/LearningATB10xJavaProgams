d162c17861de5dac1f76037309b9b768cbc17385
MaheswariBhagavathi <maheswarilearning1@gmail.com>
2025-01-17 19:56:51 +0530

    17 JAN 2025

--- src/ex_01_Java_Basic/Lab0015_Variables.java
+++ src/ex_01_Java_Basic/Lab0015_Variables.java
@@ -4,11 +4,26 @@ import org.w3c.dom.ls.LSOutput;
 
 public class Lab0015_Variables {
     public static void main(String[] args) {
-        int a = 10;
-        int age = 65;
-        age = 89;
+        //int a = 10;
+//        int age = 65;
+//        age = 89;
         int age1 = 90;
         age1 = age1+1;
+        float pi = 3.14f;
+        final int b=10;
+//       char c;
+//       System.out.println(c);
+        System.out.println(b);
+        System.out.println(pi);
+        System.out.printf("Value of pi is %f", pi);
         System.out.println(age1);
+        char c1='M';
+        char c2= 'A';
+        char c3= 'H';
+        char c4= 'I';
+        System.out.print(c1);
+        System.out.print(c2);
+        System.out.print(c3);
+        System.out.println(c4);
     }
 }
---
+++ src/ex_01_Java_Basic/Lab017_Table_of_9.java
@@ -0,0 +1,36 @@
+package ex_01_Java_Basic;
+
+public class Lab017_Table_of_9 {
+
+    public static void main(String[] args) {
+        int table = 9;
+        System.out.printf("%d x 1 = %d%n",table, table*1);
+        System.out.printf("%d x 2 = %d%n",table, table*2);
+        System.out.printf("%d x 3 = %d%n",table, table*3);
+        System.out.printf("%d x 4 = %d%n",table, table*4);
+        System.out.printf("%d x 5 = %d%n",table, table*5);
+        System.out.printf("%d x 6 = %d%n",table, table*6);
+        System.out.printf("%d x 7 = %d%n",table, table*7);
+        System.out.printf("%d x 8 = %d%n",table, table*8);
+        System.out.printf("%d x 9 = %d%n",table, table*9);
+        System.out.printf("%d x 10 = %d%n",table, table*10);
+
+        int num = 9;
+        System.out.println("Table of 9");
+        System.out.printf("9 X 1 = %d%n",num*1);
+        System.out.printf("9 X 2 = %d%n",num*2);
+        System.out.printf("9 X 3 = %d%n",num*3);
+        System.out.printf("9 X 4 = %d%n",num*4);
+        System.out.printf("9 X 5 = %d%n",num*5);
+        System.out.printf("9 X 6 = %d%n",num*6);
+        System.out.printf("9 X 7 = %d%n",num*7);
+        System.out.printf("9 X 8 = %d%n",num*8);
+        System.out.printf("9 X 9 = %d%n",num*9);
+        System.out.printf("9 X 10 = %d",num*10);
+
+
+        }
+
+
+    }
+
