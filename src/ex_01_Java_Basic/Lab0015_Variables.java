75993edbd1130db834e5cb19415a7c751d6f26fa
MaheswariBhagavathi <maheswarilearning1@gmail.com>
2025-01-17 12:40:19 +0530

    11 JAN 2025

---
+++ src/ex_01_Java_Basic/Lab0015_Variables.java
@@ -0,0 +1,14 @@
+package ex_01_Java_Basic;
+
+import org.w3c.dom.ls.LSOutput;
+
+public class Lab0015_Variables {
+    public static void main(String[] args) {
+        int a = 10;
+        int age = 65;
+        age = 89;
+        int age1 = 90;
+        age1 = age1+1;
+        System.out.println(age1);
+    }
+}
